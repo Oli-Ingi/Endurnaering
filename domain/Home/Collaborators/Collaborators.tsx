7caae03b8f9a69996393a8652db616e7fb3ac6bc
Oli <oli_ingi@msn.com>
2022-09-25 20:32:25 +0000

    Aftur laga, stafsetning

--- domain/Home/Collaborators/Collaborators.tsx
+++ domain/Home/Collaborators/Collaborators.tsx
@@ -4,7 +4,7 @@ import CollabSection from '../../../components/entities/CollabSection/CollabSect
 
 const links = [
     { logo: "/Heilsuklasinn.png", alt: "heilsuklasinn-logo-mynd", to: "https://klasinn.is" },
-    { logo: "/Heilsugaeslan_hofda.png", alt: "heilsugaesla-hofda-logo-mynd", to: "https://hgh.is" },
+    { logo: "/Heilsugaeslan_Hofda.png", alt: "heilsugaesla-hofda-logo-mynd", to: "https://hgh.is" },
     { logo: "/Virk.png", alt: "Virk-logo-mynd", to: "https://virk.is" }
 ];
 
