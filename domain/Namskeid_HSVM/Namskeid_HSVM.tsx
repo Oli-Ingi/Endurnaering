83cf5a884b0a9137c8e69473378fdc2177f88130
Oli <oli_ingi@msn.com>
2024-05-12 17:47:40 +0000

    netnámskeið

--- domain/Namskeid_HSVM/Namskeid_HSVM.tsx
+++ domain/Namskeid_HSVM/Namskeid_HSVM.tsx
@@ -30,9 +30,6 @@ const StArticleSection = styled.div`
     > div:first-child {
         flex: 1;
 
-        p {
-            margin-bottom: 15px;
-        }
     }
 
     @media screen and (max-width: 850px) {
@@ -70,6 +67,7 @@ const StSalesBox = styled.div`
     padding: 50px;
     width: 550px;
     max-width: 100%;
+    opacity: .9;
 
     @media screen and (max-width: 600px) {
         padding: 16px;
@@ -80,8 +78,15 @@ const SalesBox: FC<SalesBoxProps> = ({ badgeTitle, title, price, button }) => <S
     <StWrapper>
         <Badge color="tertiary" mb={30}>{badgeTitle}</Badge>
         <Title mb={20}>{title}</Title>
-        <Text mb={48}>
-            Heiðdís Snorradóttir, næringarfræðingur Endurnæringar stýrir 4ra vikna námskeiði þar sem farið verður yfir grunnstoðir og leiðina til heilbrigðs sambands við mat.
+        <Text mb={12}>
+            Heiðdís Snorradóttir, næringarfræðingur Endurnæringar stýrir 4ra vikna námskeiði 
+            þar sem farið verður yfir grunnstoðir og leiðina til heilbrigðs sambands við mat.
+        </Text>
+        <Text>
+            Námskeiðið er netnámskeið þar sem hópur fólks hittist í netheimum og tekur þátt í fræðslu og umræðum.
+        </Text>
+        <Text>
+            
         </Text>
         {/* <Flex width="100%" just="between" bg="primary" $wrap>
             <Text size="lg" color="primary" bold mr="1em">{price.key} <Text as="span" color="dark" size="lg">{price.value}</Text></Text>
@@ -91,12 +96,15 @@ const SalesBox: FC<SalesBoxProps> = ({ badgeTitle, title, price, button }) => <S
 
 
 const FlexHider = styled.div`
-    width: 1000px;
+    width: 1280px;
+    height: 800px;
     max-width: 100%;
+    
     background-image: url("/hsvm-heiddis.png");
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
+    background-position: 100% 100%;
     padding-top: 50px;
     padding-bottom: 50px;
     overflow: hidden;
@@ -106,7 +114,7 @@ const FlexHider = styled.div`
 const HeilbrigtSambandVidMat = () => <Column as="main">
     <FlexHider>
         <SalesBox 
-            badgeTitle="Staðnámskeið"
+            badgeTitle="Netnámskeið"
             title="Heilbrigt samband við mat"
             price={{ key: "Verð:", value: "29.900 kr." }}
             button={<Button to="https://forms.office.com/Pages/ResponsePage.aspx?id=h-GhAWgFO0uFlpl3jDbQUnTLjVvQchhNqvFinXC5IhJUNUxKRTZSMldaUFEwU0dGQlRVVEVWVTMzRyQlQCN0PWcu&fbclid=IwAR0910xS1lhnPyuvdwoWSJXOF7igLsFMtg7aqCQr45sgooGnK7MAw_inOmk">Skrá mig</Button>}
@@ -114,33 +122,63 @@ const HeilbrigtSambandVidMat = () => <Column as="main">
     </FlexHider>
     <Section asEl="div"  border="both" variant="secondary" pad="40px 1em">
         {/* <Text color="primary" bold mr="2em">Námskeið hefst: <Text as="span" color="dark">miðvikudaginn 1. mars 2023, kl. 17:30</Text></Text> */}
-        <Text color="primary" bold mr="2em">Staðsetning: <Text as="span" color="dark" >Heilsuklasinn - Bíldshöfða 9, Reykjavík</Text></Text>
+        <Text color="primary" bold mr="2em">Staðsetning: <Text as="span" color="dark" >Námskeiðið fer fram í gegnum netið.</Text></Text>
+        {/* <Text color="primary" bold mr="2em">Staðsetning: <Text as="span" color="dark" >Heilsuklasinn - Bíldshöfða 9, Reykjavík</Text></Text> */}
         <Text color="primary" bold mr="2em">Dagsetning: <Text as="span" color="dark" >Dagsetning fyrir næsta námskeið hefur ekki verið ákveðin</Text></Text>
     </Section>
     <StArticleSection>
             <div>
-                <Text>
+                <Text mb={12}>
                     Heilbrigt samband við mat byggir á þremur grunnstoðum: venjum, innsæi og næringu. 
-                    Á námskeiðinu verður farið yfir þessar grunnstoðir og hvernig þekking á þeim
-                    eykur líkur á jafnvægi í okkar mataræði.
+                    Á námskeiðinu verður farið yfir þessar grunnstoðir og hvernig þekking á þeim stuðlar
+                    að jafnvægi í okkar mataræði. Námskeiðið er þannig grunnur til þess að rýna í eigin heilsu- og matarhegðun 
+                    og hvernig við getum bætt okkar mataræði.
+                </Text>
+                <Text mb={12}>
+                    Helstu verkfæri námskeiðs eru verkefnablöð, fræðsla og umræður.
+                </Text>
+                <Text mb={12}>
+                    Farið verður yfir leiðir til þess að setja saman góðar og næringarríkar máltíðir. 
+                    Jafnframt verða kynnt mikilvæg hugtök eins og máltíðargluggar og hvernig má nýta þá sem best. 
+                    Þannig verður til sérútbúið mataræði sem er aðsniðið að hverjum og einu. 
+                    Unnið er með fyrirlestra, umræður í hópum og verkefni leyst í hverri viku. 
+                    Næringarlæsi er eflt með því að kenna álestur umbúða af matvælum og auka þannig meðvitund 
+                    um hvað maturinn gefur okkur. Áhersla er lögð á að átta sig á hvað maturinn gefur 
+                    líkamlega og andlega og kynnast mat á annan hátt en með því að telja hitaeiningar eða vigta. 
+                    Farið er yfir helstu mýtur þegar kemur að mat og hvað það felur í sér að eiga í heilbrigðu sambandi við mat. 
+                    Allt efni námskeiðsins er byggt á ritrýndum heimildum og nýjustu þekkingu hverju sinni þegar kemur að mataræði.
+                </Text>
+                <Text mb={0}>
+                    Jafnframt er lögð áhersla á svokallaða "þyngdarhlutlausa nálgun" en í ljósi þess hve 
+                    rík megrunarmenning er þá er námskeiðið ekki ætlað sem skyndikúr eða skyndilausn. 
+                    Rannsóknir hafa sýnt að ef einblínt er einungis á þyngd þá geti það skapað 
+                    óheilbrigðar matarvenjur og heilsubrest.
                 </Text>
                 <Text>
-                    Námskeiðið hentar fólki sem langar að ná betri tökum á mataræði- og venjum.
+                    Önnur markmið eru möguleg og þá með tilliti til meiri orku, betri lífsgæða, betri meltingar, 
+                    minni streitu og kvíða. Þannig ná einstaklingar að setja sér markmið sem endast út ævina 
+                    í stað þess að þau renni út á tíma. 
                 </Text>
+                <Text mb={24}>
+                    Námskeiðið er í gangi í 4 vikur og fer fram á Kara Connect.
+                </Text>
+                <Text mb={24}>Námskeiðið hentar öllu fólki sem langar að ná betri tökum á mataræði- og venjum.</Text>
                 <Title variant="subsection">Helstu markmið námskeiðsins:</Title>
                 <BulletList>
                     <Text as="li">
-                        Styrkja traust á eigið matarinnsæi sem getur leitt til minni streitu, kvíða og samviskubits
-                        sem við finnum oft fyrir í tengslum við mat.
+                        Setja sér raunhæf markmið sem endast til lengri tíma útfrá daglegri rútínu og heilsufari.
                     </Text>
                     <Text as="li">
-                        Farið verður yfir leiðir til þess að setja saman góðar og næringarríkar máltíðir.
+                    Styrkja traust á eigið matarinnsæi sem getur leitt til minni streitu, kvíða og samviskubits sem við finnum oft fyrir í tengslum við mat.
                     </Text>
                     <Text as="li">
                         Skoðum hvernig við getum greint matarhegðun okkar og tileinkað okkur betri matarvenjur.
                     </Text>
+                    <Text as="li">
+                        Öðlumst aukið næringar- og matvælalæsi sem skilar sér í meðvitaðri ákvörðunum tengt fæðuvali.
+                    </Text>
                 </BulletList>
-                <Line mb={24} />
+                {/* <Line mb={24} />
                 <Title variant="subsection" mb={24}>Skilmálar og almennar upplýsingar</Title>
                 <Text>
                     Námskeiðið er 4ra vikna.
@@ -156,7 +194,7 @@ const HeilbrigtSambandVidMat = () => <Column as="main">
                     Heiðdís Snorradóttir næringarfræðingur M.Sc. stýrir námskeiðinu og
                     veitir nánari upplýsingar ef þess er óskað í gegnum netfangið <Link inline to="mailto:heiddis@endurnaering.is" permaLine>heiddis@endurnaering.is</Link> eða
                     í síma <Link inline permaLine to="tel:8487407">848-7407</Link>.
-                </Text>
+                </Text> */}
             </div>
             {/* <DescCard 
                 date="15. febrúar 2023, 17:00-19:00" 
